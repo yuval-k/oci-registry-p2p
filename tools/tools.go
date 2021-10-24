3db7af8ae84d29ff03f30d1d93ad8b58f0cc370a
Yuval Kohavi <yuval.kohavi@gmail.com>
2021-10-24 19:28:35 -0400

    tools

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,8 @@
+// +build tools
+
 // this package should not be used by other packages. just here to have go mod vendor stuff.
 package tools
 
 import (
-	"github.com/goreleaser/goreleaser"
+	_ "github.com/goreleaser/goreleaser"
 )
