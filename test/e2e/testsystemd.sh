48986c318e5bc34edf287f4557c9568138334beb
Yuval Kohavi <yuval.kohavi@gmail.com>
2021-11-27 08:00:39 -0500

    test

--- test/e2e/testsystemd.sh
+++ test/e2e/testsystemd.sh
@@ -8,7 +8,8 @@ go build -o ${PROJ_DIR}/oci-registry-p2p ${PROJ_DIR}
 # build using stdin, as we dont need build context.
 cat ${PROJ_DIR}/scripts/Dockerfile.systemdtest | podman build -f - -t testsystemd 
 
-podman run -d -p 5000:5000 --rm --name systemdtest -v ${PROJ_DIR}/oci-registry-p2p:/usr/local/bin/oci-registry-p2p:ro \
+# don't even bother with "-p 5000:5000", as we just test that systemd can start us
+podman run -d --rm --name systemdtest -v ${PROJ_DIR}/oci-registry-p2p:/usr/local/bin/oci-registry-p2p:ro \
     -v ${PROJ_DIR}/scripts/example-config.yaml:/etc/oci-registry-p2p/config.yaml:ro \
     -v ${PROJ_DIR}/test/e2e/cert.pem:/etc/oci-registry-p2p/cert.pem:ro \
     -v ${PROJ_DIR}/test/e2e/key.pem:/etc/oci-registry-p2p/key.pem:ro \
