7e8bced297353344d8a0120392e18b3fbe33918c
Yuval Kohavi <yuval.kohavi@gmail.com>
2021-11-28 10:50:48 -0500

    add logs for debug

--- test/e2e/testsystemd.sh
+++ test/e2e/testsystemd.sh
@@ -10,19 +10,21 @@ go build -o ${PROJ_DIR}/oci-registry-p2p ${PROJ_DIR}
 cat ${PROJ_DIR}/scripts/Dockerfile.systemdtest | podman build -f - -t testsystemd 
 
 # don't even bother with "-p 5000:5000", as we just test that systemd can start us
-CID=$(podman run -d --rm --name systemdtest -v ${PROJ_DIR}/oci-registry-p2p:/usr/local/bin/oci-registry-p2p:ro \
+CID=$(podman run -d -v ${PROJ_DIR}/oci-registry-p2p:/usr/local/bin/oci-registry-p2p:ro \
     -v ${PROJ_DIR}/scripts/example-config.yaml:/etc/oci-registry-p2p/config.yaml:ro \
     -v ${PROJ_DIR}/test/e2e/cert.pem:/etc/oci-registry-p2p/cert.pem:ro \
     -v ${PROJ_DIR}/test/e2e/key.pem:/etc/oci-registry-p2p/key.pem:ro \
     -v ${PROJ_DIR}/install/oci-registry-p2p.service:/etc/systemd/system/oci-registry-p2p.service:ro \
     testsystemd)
 
-trap "podman stop $CID" EXIT
+trap "podman rm -f $CID" EXIT
 
 podman exec $CID /bin/sh -c "systemctl daemon-reload && systemctl enable oci-registry-p2p && systemctl start oci-registry-p2p"
 
-
+# logs:
+echo "Logs:"
+podman exec $CID /bin/sh -c "systemctl status oci-registry-p2p"
 podman exec $CID /bin/sh -c "journalctl -u oci-registry-p2p"
-
+podman logs $CID
 # registry will fail to start because ipfs is not there. that's ok, as we are just testing that systemd
 # is able to start it. if in the future we can get it to work, we can try "curl localhost:5000/v2/_catalog" to confirm.
